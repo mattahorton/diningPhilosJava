2abf24e99797ea7a0f7bc201fc6c78a5c6f69e36
mattahorton <matthew.a.horton@gmail.com>
2013-10-21 14:47:50 -0500

    More complete Philosopher class

--- Philosopher.java
+++ Philosopher.java
@@ -1,41 +1,73 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class Philosopher implements Runnable {
 	
 	Waiter waiter;
-	int philosopher;
-	int numEat, numThink;
+	int identifier;
+	int numEat = 0;
+	int numThink = 0;
+	long elapsed,elapEat;
+	static SimpleDateFormat sdf = new SimpleDateFormat("hh:mm");
 	
-	public Philosopher(Waiter waiter, int philosopher){
+	public Philosopher(Waiter waiter, int identifier){
 		this.waiter = waiter;
-		this.philosopher = philosopher;
+		this.identifier = identifier;
 	}
 
 	@Override
 	public void run() {
+		String req, eat, finish;
 		
 		while(!Thread.currentThread().isInterrupted()){
 			try {
 				think();
-				waiter.pickup();
+				req = getClockTime();
+				waiter.pickup(identifier);
+				eat = getClockTime();
 				eat();
-				waiter.putdown();
+				finish = getClockTime();
+				printEat(identifier,numThink,elapEat,req,eat,finish);
+				waiter.putdown(identifier);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}		
 	}
 
 	private void think() throws InterruptedException {
+		numThink++;
+		long start = System.currentTimeMillis();
 		sleepTime(500);
+		elapsed = System.currentTimeMillis() - start;
+		printThink(identifier,numThink,elapsed);
 	}
 	
 	private void eat() throws InterruptedException {
+		numEat++;
+		long start = System.currentTimeMillis();
 		sleepTime(500);
+		elapEat = System.currentTimeMillis() - start;
+	}
+	
+	private static String getClockTime() {
+		Date date = new Date();
+		String stringDate = sdf.format(date);
+		return stringDate;
 	}
 	
 	private void sleepTime(int time) throws InterruptedException {
 		long ms = (long)(time*Math.random());
 		Thread.sleep(ms);
+	}
+	
+	private void printThink(int i, int count, long time) {
+		System.out.println(count + ": Philosopher " + i + " has been thinking for " + time/1000 + " seconds.");
+	}
+	
+	private void printEat(int i, int count, long time, String reqTime, String eatTime, String finishTime) {
+		System.out.println(count + ": Philosopher " + i + " requested to eat at " + reqTime + ", started eating at "
+				+ eatTime + ", finished eating at " + finishTime + ", and ate for " + time/1000 + " seconds.");
 	}
 }
--- Waiter.java
+++ Waiter.java
@@ -1,18 +1,86 @@
 
 public class Waiter {
-
-	public Waiter(){
-		
+	
+	//State enum
+	private enum State {
+		HUNGRY, EATING, THINKING
 	}
 
-	public void pickup() {
-		// TODO Auto-generated method stub
-		
+	private State[] state;
+	boolean[] leftHungry, rightHungry;
+	private int philoNum;
+	
+	//Waiter constructor
+	public Waiter(int philoNum){
+		this.philoNum = philoNum;
+		state  = new State[philoNum];
+		for(int i = 0; i < philoNum; i++) {
+			state[i] = State.THINKING;
+			leftHungry[i] = false;
+			rightHungry[i] = false;
+		}
 	}
 
-	public void putdown() {
-		// TODO Auto-generated method stub
-		
+	//Request to eat
+	public synchronized void pickup(int i) throws InterruptedException {
+		setState(i,State.HUNGRY);
+		test(i);
+		if(state[i] != State.EATING)
+			this.wait();
+		rightHungry[leftPhilo(i)] = false;
+	    leftHungry[rightPhilo(i)] = false;
 	}
 
+	//Signal for having finished eating
+	public synchronized void putdown(int i) {
+		setState(i,State.THINKING);
+		test(leftPhilo(i));
+		if (state[leftPhilo(i)] == State.HUNGRY)
+	         leftHungry[i] = true;
+		test(rightPhilo(i));
+		if (state[rightPhilo(i)] == State.HUNGRY)
+	         rightHungry[i] = true;
+	}
+	
+	//Test for permission to eat/availability of chopsticks
+	private synchronized void test(int i) {
+		if  ((state[rightPhilo(i)] != State.EATING) && 
+		           (state[i] == State.HUNGRY) &&
+		           (state[leftPhilo(i)] != State.EATING) &&
+		           !leftHungry[i] && !rightHungry[i]) {
+			state[i] = State.EATING;
+		    this.notifyAll();
+		}
+	}
+
+	//State getter
+	public State getState(int i) {
+		return state[i];
+	}
+
+	public void setState(int i, State newState) {
+		state[i] = newState;
+		sanityCheck();
+	}
+
+	private int leftPhilo(int i) {
+		return (i + philoNum - 1) % philoNum;
+	}
+	
+	private int rightPhilo(int i) {
+		return (i + 1) % philoNum;
+	}
+	
+	//Borrowed from CWRU EECS338 example solution
+	//Makes sure we don't have two consecutive philosophers eating
+    private void sanityCheck() {
+        for(int i = 0; i < philoNum; i++) {
+            if(state[i] == State.EATING &&
+               (state[ leftPhilo(i)] == State.EATING ||
+                state[rightPhilo(i)] == State.EATING))
+            {
+                throw new IllegalStateException();
+            }
+        }
+    }
 }
